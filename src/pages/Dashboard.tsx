87ee262b546b4b04ac7baf794543c9194f9d95c6
Maksym Menzheha <masimfyd@gmail.com>
2022-02-12 19:15:00 +0200

    add logout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { gql, useMutation } from "urql";
 import { useIsAuth } from "../utils/useIsAuth";
+
+const LogoutMutation = gql`
+  mutation Logout {
+    logout
+  }
+`;
 
 const Dashboard: React.FC = () => {
   useIsAuth();
+
+  const [{ fetching }, logout] = useMutation(LogoutMutation);
+
+  const navigate = useNavigate();
+
   return (
     <>
-      <Container className="w-100">hello</Container>
+      <Container className="w-100">
+        hello
+        <Button
+          disabled={fetching}
+          variant="primary"
+          className="w-100"
+          onClick={async () => {
+            await logout();
+            navigate("/login", { replace: true });
+          }}
+        >
+          Logout
+        </Button>
+      </Container>
     </>
   );
 };
