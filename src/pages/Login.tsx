57a0642e45019ec83a80cc6d541455f6860f6fad
Maksym Menzheha <masimfyd@gmail.com>
2022-02-12 17:43:18 +0200

    Login page urql, errors

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,45 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import { Button, Form, Container } from "react-bootstrap";
+import { gql, useMutation } from "urql";
+
+const LoginMutaion = gql`
+  mutation Login($input: UserInput!) {
+    login(input: $input) {
+      user {
+        id
+        username
+      }
+      errors {
+        field
+        message
+      }
+    }
+  }
+`;
 
 const Login: React.FC = () => {
   const usernameRef = React.createRef<HTMLInputElement>();
   const passwordRef = React.createRef<HTMLInputElement>();
 
+  const [, executeMutation] = useMutation(LoginMutaion);
+
+  const [errors, setErrors] = useState<{ field: string; message: string }>();
+
   const onLoginSubmit = (e: FormEvent) => {
     e.preventDefault();
+
+    const input = {
+      username: usernameRef.current?.value,
+      password: passwordRef.current?.value,
+    };
+    executeMutation({ input }).then((res) => {
+      if (res.data.login.errors[0]) {
+        setErrors(res.data.login.errors[0]);
+      } else {
+        //push user to ...
+      }
+    });
+
     console.log("username: ", usernameRef.current?.value);
     console.log("password: ", passwordRef.current?.value);
   };
@@ -22,6 +55,9 @@ const Login: React.FC = () => {
               placeholder="enter username"
               ref={usernameRef}
             />
+            {errors && errors.field === "username" ? (
+              <Form.Text className="text-danger">{errors.message}</Form.Text>
+            ) : null}
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Password</Form.Label>
@@ -30,6 +66,9 @@ const Login: React.FC = () => {
               placeholder="password"
               ref={passwordRef}
             />
+            {errors && errors.field === "password" ? (
+              <Form.Text className="text-danger">{errors.message}</Form.Text>
+            ) : null}
           </Form.Group>
           <Button className="mt-3" variant="primary" type="submit">
             Submit
