0f20209cef00602a42c8de9ffb5b37054eff71ae
Maksym Menzheha <masimfyd@gmail.com>
2022-02-15 16:34:28 +0200

    add better update query and login exchange

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,5 +1,8 @@
 import { cacheExchange } from "@urql/exchange-graphcache";
 import { createClient, dedupExchange, fetchExchange } from "urql";
+import { LoginMutation, MeDocument, MeQuery } from "../generated/graphql";
+import { betterUpdateQuery } from "./betterUpdateQuery";
+
 export const createUrqlClient = createClient({
   url: "http://localhost:3007/graphql",
   fetchOptions: () => {
@@ -7,5 +10,30 @@ export const createUrqlClient = createClient({
       credentials: "include" as const,
     };
   },
-  exchanges: [dedupExchange, cacheExchange({}), fetchExchange],
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
