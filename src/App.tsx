666a01789fd036175c9895d71a18ea0b107e3bff
Maksym Menzheha <masimfyd@gmail.com>
2022-02-15 14:22:54 +0200

    redirect from / to /d and fix login caching issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Provider } from "urql";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
@@ -12,6 +12,7 @@ function App() {
     <Provider value={createUrqlClient}>
       <Container fluid>
         <Routes>
+          <Route path="/" element={<Navigate to="/d" />} />
           <Route path="/d" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
